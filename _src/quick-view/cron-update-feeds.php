5ea2aefc41b793ec12a2fada4e4b7f11c9ddeabd
davidmars <d.marsalone@gmail.com>
2019-01-08 09:08:54 +0100

    et oui le sdk facebook peut renvoyer des notices...no comment :(

--- _src/quick-view/cron-update-feeds.php
+++ _src/quick-view/cron-update-feeds.php
@@ -3,7 +3,10 @@
  * Ce script met à jour les feeds depuis l'api Facebook
  */
 
+//et oui le sdk facebook peut renvoyer des notices...no comment :(
+error_reporting(E_ALL ^ E_NOTICE);
 use Classiq\Models\SocialFeed;
+
 cq()->configPreventDbNotifications = true;
 /** @var Socialfeed[] $feeds */
 $feeds=db()->find("socialfeed","ORDER BY date_modified ASC"); //les plus vieux en premier
